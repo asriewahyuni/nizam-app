8831d26edf8a6740546193066782c24fcd2e2c37
asriewahyuni <asrie.wahyuni@gmail.com>
2026-04-14 19:05:57 +0700

    chore: remove stale debug/test scripts

--- scripts/test_aging.ts
+++
@@ -1,5 +0,0 @@
-import { getAgingSummary } from '../modules/accounting/actions/aging.actions'
-
-async function run() {
-  const orgId = "1a8b981f-7e83-4a1c-b26a-9a9ed71110af" // Need to find a real orgId. Let's find one.
-}
