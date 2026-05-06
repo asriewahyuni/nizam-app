95b680fb958182850b946fdaed16025e592f144d
idyogi <yogyan12@gmail.com>
2026-05-06 18:21:42 +0700

    Add Microsoft Clarity tracking

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -5,8 +5,15 @@
  */
 
 import * as Sentry from '@sentry/nextjs'
+import Clarity from '@microsoft/clarity'
 import { getClientSentryOptions } from '@/lib/monitoring/sentry'
 
 Sentry.init(getClientSentryOptions())
+
+const clarityProjectId = process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID?.trim()
+
+if (clarityProjectId) {
+  Clarity.init(clarityProjectId)
+}
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
